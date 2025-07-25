2321cfa770e3112c65c8f2557ba47c73f06f3a08
Hamon_Soares <64448739+hamonsoares@users.noreply.github.com>
2025-07-24 22:03:54 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,7 +1,7 @@
 <?php
 define('HOST', '127.0.0.1');
-define('USUARIO', 'root');
-define('SENHA', '123456');
+define('USUARIO', 'user');
+define('SENHA', 'pass');
 define('DB', 'login');
 
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, DB) or die ('Não foi possível conectar');
